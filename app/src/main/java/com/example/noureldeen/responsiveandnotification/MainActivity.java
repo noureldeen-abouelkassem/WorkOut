824a5906431bb2dd32bf0e7e00b799523ab6911e
noureldeen <levn.noureldeen@outlook.com>
2017-09-12 01:58:06 +0200

    our app works well on phone and details activity shows description of each item

--- app/src/main/java/com/example/noureldeen/responsiveandnotification/MainActivity.java
+++ app/src/main/java/com/example/noureldeen/responsiveandnotification/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.noureldeen.responsiveandnotification;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     public ListView myList;
+    AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Intent intent = new Intent(MainActivity.this,DetailsActivity.class);
+            intent.putExtra(DetailsActivity.ItemID,i);
+            startActivity(intent);
+        }
+    };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myList = (ListView) findViewById(R.id.list_view_mylist);
-        ArrayAdapter<String> myListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 ,getItemsname());
+        ArrayAdapter<String> myListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getItemsname());
         myList.setAdapter(myListAdapter);
+        myList.setOnItemClickListener(onItemClickListener);
     }
 
     // we use this function to fill an array of names of our items
