2cd212c928b7c6d56efd9956c19c53de62f57e08
noureldeen <levn.noureldeen@outlook.com>
2017-09-12 03:50:45 +0200

    now we use notifications :)

--- app/src/main/java/com/example/noureldeen/responsiveandnotification/DetailsActivity.java
+++ app/src/main/java/com/example/noureldeen/responsiveandnotification/DetailsActivity.java
@@ -1,14 +1,34 @@
 package com.example.noureldeen.responsiveandnotification;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
     public static String ItemID = "id";
     TextView itemDescription;
+    Button showNotification ;
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int mNotificationId = 1;
+            NotificationCompat.Builder mBuilder =
+                    (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext())
+                            .setSmallIcon(R.mipmap.notification_icon)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!");
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+            mNotificationManager.notify(mNotificationId, mBuilder.build());
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +36,7 @@ public class DetailsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         itemDescription = (TextView) findViewById(R.id.textView_itemDescription);
         itemDescription.setText(MyList.myList[intent.getIntExtra(ItemID,0)].getDescription());
+        showNotification = (Button) findViewById(R.id.button_showNotification);
+        showNotification.setOnClickListener(onClickListener);
     }
 }
--- app/src/main/java/com/example/noureldeen/responsiveandnotification/ItemDescriptionFragment.java
+++ app/src/main/java/com/example/noureldeen/responsiveandnotification/ItemDescriptionFragment.java
@@ -1,11 +1,15 @@
 package com.example.noureldeen.responsiveandnotification;
 
 import android.app.Fragment;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -15,6 +19,22 @@ import android.widget.TextView;
 public class ItemDescriptionFragment extends Fragment {
     TextView itemDescription;
     private int ItemID = 0;
+    Button showNotification;
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int mNotificationId = 1;
+            NotificationCompat.Builder mBuilder =
+                    (NotificationCompat.Builder) new NotificationCompat.Builder(getActivity())
+                            .setSmallIcon(R.mipmap.notification_icon)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!");
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+
+            mNotificationManager.notify(mNotificationId, mBuilder.build());
+        }
+    };
 
     @Nullable
     @Override
@@ -25,6 +45,8 @@ public class ItemDescriptionFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_itemdetails, container, false);
         itemDescription = (TextView) view.findViewById(R.id.textView_itemDescription);
         itemDescription.setText(MyList.myList[ItemID].getDescription());
+        showNotification = (Button) view.findViewById(R.id.button_showNotification);
+        showNotification.setOnClickListener(onClickListener);
         return view;
     }
 
